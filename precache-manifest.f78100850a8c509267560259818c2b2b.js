5e8148ab4f9b9fc84db2f11f098a35b5dc1832b6
ianchouinard <ianchouinard@gmail.com>
2019-06-20 19:23:05 -0400

    Updates

---
+++ precache-manifest.f78100850a8c509267560259818c2b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e003cc0573ae31ad96",
+    "url": "/DesktopUI/static/js/runtime~main.d6f30a7a.js"
+  },
+  {
+    "revision": "d1daf7d313fbd18d1cf4",
+    "url": "/DesktopUI/static/js/main.6b2b5353.chunk.js"
+  },
+  {
+    "revision": "0ba3773068713ac13844",
+    "url": "/DesktopUI/static/js/2.82a396b7.chunk.js"
+  },
+  {
+    "revision": "d1daf7d313fbd18d1cf4",
+    "url": "/DesktopUI/static/css/main.f142a84a.chunk.css"
+  },
+  {
+    "revision": "0ba3773068713ac13844",
+    "url": "/DesktopUI/static/css/2.5722b5d4.chunk.css"
+  },
+  {
+    "revision": "327feda1a904cfb589552779324ecb8a",
+    "url": "/DesktopUI/index.html"
+  }
+];
